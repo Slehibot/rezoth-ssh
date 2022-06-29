ce1fbc92076186ac44bf3c42f2859789dd024f80
Slehibot <88370866+Slehibot@users.noreply.github.com>
2022-06-29 14:50:11 +0530

    Update UserManager.sh

--- etc/UserManager.sh
+++ etc/UserManager.sh
@@ -9,6 +9,10 @@ ENDCOLOR="\e[0m"
 
 clear
 
+#public ip
+
+pub_ip=$(wget -qO- https://ipecho.net/plain ; echo)
+
 #add users
 
 echo -ne "${YELLOW}Enter the username : "; read username
@@ -24,11 +28,34 @@ echo -ne "Enter No. of Days till expiration : ";read nod
 exd=$(date +%F  -d "$nod days")
 useradd -e $exd -M -N -s /bin/false $username && echo "$username:$password" | chpasswd &&
 clear &&
-echo -e "${GREEN}User Detail" &&
-echo -e "${RED}-----------" &&
-echo -e "${GREEN}\nUsername :${YELLOW} $username" &&
-echo -e "${GREEN}\nPassword :${YELLOW} $password" &&
-echo -e "${GREEN}\nExpire Date :${YELLOW} $exd ${ENDCOLOR}" ||
+
+echo -e "${YELLOW}================================      " &&
+echo ""
+echo ""
+echo ""
+echo -e "${YELLOW}================================      " &&
+echo -e "${GREEN}   ❣️ㅤSLEHIBOT VPS MANAGERㅤ❣️         "    &&
+echo -e "${YELLOW}================================      " &&
+echo ""
+echo -e "${GREEN}❖─────༻  SSH ACCOUNT ༺─────❖      " &&
+echo ""
+echo -e "${GREEN}\nIP/Host   :${YELLOW} $pub_ip" &&
+echo -e "${GREEN}Username    :${YELLOW} $username" &&
+echo -e "${GREEN}Password    :${YELLOW} $password" &&
+echo -e "${GREEN}Expire Date :${YELLOW} $exd ${ENDCOLOR}" &&
+echo ""
+
+echo -e "${GREEN}☬ SLEHIBOT Auto Script Ports ☬     "  &&
+echo ""
+echo -e "${GREEN} ★ OpenSSH Port   : 22     " &&
+echo -e "${GREEN} ★ SSL Port       : 443     " &&
+echo -e "${GREEN} ★ Dropbear Port  : 80     " &&
+echo -e "${GREEN} ★ Proxy Port     : 8080     " &&
+echo -e "${GREEN} ★ Badvpn         : 7300     " &&
+echo -e "${GREEN}❖───── ⍨ ──── ⍤ ──── ⍨ ─────❖     " &&
+echo -e "${GREEN} ☬❦─ ⍣ LAKMAL 〄 SANDARU ⍣ ─❦☬     " &&
+echo -e "${GREEN}❖───── ⍨ ──── ⍤ ──── ⍨ ─────❖    " &&
+echo -e "${GREEN} ☬[⍣] ꧁ SLEHIBOT TEAM ꧂ [⍣]☬     " ||
 echo -e "${RED}\nFailed to add user $username please try again.${ENDCOLOR}"
 
 #return to panel
